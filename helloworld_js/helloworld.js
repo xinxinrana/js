a7e3622eaf9983c2f01344301c256647c8623f6d
xinxinrana <top.xiong@foxmail.com>
2022-10-02 23:27:57 +0800

    油猴脚本信息

--- helloworld_js/helloworld.js
+++ helloworld_js/helloworld.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    top.xiong@foxmail.com
+// @version      0.1
+// @description  学习
+// @author       XY
+// @match        *://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+
 (function() {
     'use strict';
 
